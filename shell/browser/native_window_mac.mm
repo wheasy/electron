0282ca4a5d027612b3fb35674a3d87584ee05bac
deepak1556 <hop2deep@gmail.com>
2021-06-23 00:19:19 -0700

    fix: window ordering on mac

--- shell/browser/native_window_mac.mm
+++ shell/browser/native_window_mac.mm
@@ -1647,8 +1647,6 @@ void NativeWindowMac::NotifyWindowWillLeaveFullScreen() {
 }
 
 void NativeWindowMac::SetActive(bool is_key) {
-  if (is_key)
-    widget()->Activate();
   is_active_ = is_key;
 }
 
